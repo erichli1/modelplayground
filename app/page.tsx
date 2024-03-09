28124a8252e9593910e6ae132e816b77295892ce
erichli1 <ehli@college.harvard.edu>
2024-03-09 13:04:04 -0800

    Add initial model sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,18 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Card, CardContent } from "@/components/ui/card";
+import { CircleMinus, CirclePlus, Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const API_PROVIDERS = [
   { label: "OpenAI", keyName: "OPENAI_API_KEY" },
@@ -17,8 +29,22 @@ const API_PROVIDERS = [
     keyName: "ANTHROPIC_API_KEY",
   },
 ];
-import { Eye, EyeOff } from "lucide-react";
-import { useState } from "react";
+
+const MODELS = [
+  {
+    llm: "gpt-4-turbo-0125",
+    provider: "OpenAI",
+  },
+  {
+    llm: "claude-3-opus",
+    provider: "Anthropic",
+  },
+];
+
+type Model = {
+  llm: string;
+  provider: string;
+};
 
 export default function Home() {
   return (
@@ -30,17 +56,15 @@ export default function Home() {
             <APIKeysPanel providers={API_PROVIDERS} />
           </ResizablePanel>
           <ResizableHandle />
-          <ResizablePanel defaultSize={50} className="flex flex-col">
+          <ResizablePanel defaultSize={40} className="flex flex-col">
             <div className="flex-grow"></div>
-            <div className="px-4 py-2 border-t">
+            <div className="px-4 py-2 border-t flex justify-end">
               <Button>Run</Button>
             </div>
           </ResizablePanel>
           <ResizableHandle />
-          <ResizablePanel defaultSize={30}>
-            <div className="flex h-[200px] items-center justify-center p-6">
-              <span className="font-semibold">One</span>
-            </div>
+          <ResizablePanel defaultSize={40}>
+            <ComparePanel models={MODELS} />
           </ResizablePanel>
         </ResizablePanelGroup>
       </main>
@@ -59,7 +83,7 @@ function APIKeysPanel({
     <ScrollArea className="h-full p-4 overflow-auto">
       <div className="flex flex-col gap-4">
         <div className="flex flex-row gap-1 items-center">
-          <span className="font-semibold">API Keys</span>
+          <span className="font-semibold">Add API keys</span>
           <Button
             variant="ghost"
             size="icon"
@@ -80,6 +104,84 @@ function APIKeysPanel({
             <Label>{provider.label}</Label>
             <Input type={showKeys ? "text" : "password"} />
           </div>
+        ))}
+      </div>
+    </ScrollArea>
+  );
+}
+
+function ComparePanel({ models }: { models: Array<Model> }) {
+  const [selectedModel, setSelectedModel] = useState<string>("");
+  const [modelsToCompare, setModelsToCompare] = useState<
+    Array<Model & { id: string }>
+  >([]);
+
+  return (
+    <ScrollArea className="h-full p-4 overflow-auto">
+      <div className="flex flex-col gap-2">
+        <p className="font-semibold">Select models</p>
+        <div className="p-0.5 flex flex-row gap-1">
+          <Select
+            value={selectedModel}
+            onValueChange={(newVal) => setSelectedModel(newVal)}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select a model" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {models.map((model) => (
+                  <SelectItem
+                    value={`${model.provider}-${model.llm}`}
+                    key={`selectModelToAdd-${model.provider}-${model.llm}`}
+                  >
+                    {`${model.llm} (${model.provider})`}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <Button
+            size="icon"
+            variant="ghost"
+            disabled={selectedModel === ""}
+            onClick={() => {
+              const foundModel = models.find(
+                (model) => selectedModel === `${model.provider}-${model.llm}`
+              );
+              if (foundModel) {
+                setModelsToCompare([
+                  ...modelsToCompare,
+                  { ...foundModel, id: uuidv4() },
+                ]);
+                setSelectedModel("");
+              }
+            }}
+          >
+            <CirclePlus className="h-4 w-4" />
+          </Button>
+        </div>
+
+        {modelsToCompare.map((model, index) => (
+          <Card key={`modelCompare-${model.provider}-${model.llm}-${index}`}>
+            <CardContent className="text-sm p-4">
+              <div className="flex flex-row justify-between items-center">
+                <p className="font-bold">{`${model.llm} (${model.provider})`}</p>
+                <Button
+                  size="icon"
+                  variant="ghost"
+                  className="h-5 w-5"
+                  onClick={() =>
+                    setModelsToCompare(
+                      modelsToCompare.filter((m) => m.id !== model.id)
+                    )
+                  }
+                >
+                  <CircleMinus className="h-4 w-4" />
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
         ))}
       </div>
     </ScrollArea>
