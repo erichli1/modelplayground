db63049cd9df81f37a18afaa964f9e19865408b5
erichli1 <ehli@college.harvard.edu>
2024-03-15 16:12:00 -0700

    Update metadata + add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import ConvexClientProvider from "./ConvexClientProvider";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My App Title",
-  description: "My app description",
+  title: "model playground",
+  description: "Compare LLMs and providers based on quality, speed, and cost.",
 };
 
 export default function RootLayout({
@@ -17,6 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-EFDTNB807T" />
+      <Script id="google-analytics">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-EFDTNB807T');
+        `}</Script>
       <body className={inter.className}>
         <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
